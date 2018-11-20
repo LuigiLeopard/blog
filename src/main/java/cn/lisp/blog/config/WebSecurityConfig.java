80a1bf89f4092bbc36689033cb000c16036d7376
LuigiLeopard <shpli92@163.com>
2018-11-20 18:06:43 +0800

    增加登陆校验

---
+++ src/main/java/cn/lisp/blog/config/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package cn.lisp.blog.config;
+
+/**
+ * @ClassName: WebSecurityConfig
+ * @Description: TODO
+ * @author: lisp
+ * @date: 2018/11/20 下午 5:54
+ * @version: v1.0
+ */
+public class WebSecurityConfig {
+}
---
+++ src/main/java/cn/lisp/blog/controller/LoginController.java
@@ -0,0 +1,20 @@
+package cn.lisp.blog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: LoginController
+ * @Description: TODO
+ * @author: lisp
+ * @date: 2018/11/20 下午 5:47
+ * @version: v1.0
+ */
+@RestController
+public class LoginController {
+
+    @RequestMapping("/")
+    public String login(){
+        return "home";
+    }
+}
