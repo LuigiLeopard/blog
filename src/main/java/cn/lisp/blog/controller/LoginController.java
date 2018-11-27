52189cacc0c64b795e80112f13048c134d6f38cc
LuigiLeopard <shpli92@163.com>
2018-11-27 10:46:37 +0800

    增加首页和打包为war包

--- src/main/java/cn/lisp/blog/controller/LoginController.java
+++ src/main/java/cn/lisp/blog/controller/LoginController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     @RequestMapping("/")
-    public String login(){
+    public String index(){
         return "home";
     }
 }
